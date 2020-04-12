28bf29eeefff66343fa0b3a1c4775b4b815c776d
dc <chendana95@gmail.com>
2020-04-11 23:22:13 -0400

    initial commit

---
+++ build/precache-manifest.169b648d72739a5164db3d1690da3576.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38336561ec39950979e2676ac96fa29c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "daa3e2b01d16a4f55161",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c01bca8b862f0515484f",
+    "url": "/static/js/2.0b10097f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0b10097f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "daa3e2b01d16a4f55161",
+    "url": "/static/js/main.79ba2f95.chunk.js"
+  },
+  {
+    "revision": "dfc4bf6d6f6f56f18e0f",
+    "url": "/static/js/runtime-main.ef3d3713.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
